c5d7392313e2ad83939abbf9c0ca63b709a38e2f
abnuo <66645994+abnuo@users.noreply.github.com>
2021-10-23 22:57:56 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request, abort, Response, redirect, jsonify, send_from_directory
 import os
+import sqlite3
 import logging
 app = Flask(__name__, static_folder='/')
 logging.basicConfig(level=logging.INFO)
 
 @app.route('/hello')
 def index():
+    return "Hello World!"
+
+@app.route('/add')
+def index():
+    #if request.args.get('e', default=None) != None:
+    exprC = request.args.get('e', default="v3b64q1ZKzk9JVbJS0ijRKtE3NTCwszNVAzE0lWoB")
+    title = request.args.get('t', default="Untitled song")
+    con = sqlite3.connect('bytebeats.db')
+    cur = con.cursor()
+    cur.execute("INSERT INTO bytebeats VALUES (?,?)", (title, exprC))
+    cur.close()
     return "Hi"
 
 #port = int(os.environ.get("PORT", 5000))
